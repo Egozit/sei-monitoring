ce7d460cf061a817ff49e8b8b5da2e0cf4a3717e
NktSH <49846495+NktSH@users.noreply.github.com>
2022-07-15 15:47:29 +0500

    Add files via upload

---
+++ variables.sh
@@ -0,0 +1,4 @@
+#Sei monitoring variables template 
+COS_BIN_NAME=insert_path_to_sei_binary # example: /root/go/bin/seid or /home/user/go/bin/seid
+COS_PORT_RPC=insert_port_here              # default: 12657
+COS_VALOPER=insert_valoper_address         # example: seivaloper1234545636767376535673
